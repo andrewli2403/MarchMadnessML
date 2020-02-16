5b685e17e72d9ba5e77a22c0df4007cf65d8b8e7
Andrew Li <Andrew@Andrews-Air.attlocal.net>
2020-02-16 09:47:41 -0800

    parameter tuning

---
+++ HyperParameterTuning.py
@@ -0,0 +1,39 @@
+#import
+import numpy as np
+
+import torch
+from torch.autograd import Variable
+from torch import optim
+
+from UtilityFunctions import load_marchmadness
+from UtilityFunctions import build_model
+from UtilityFunctions import train
+from UtilityFunctions import predict
+
+
+def main():
+    torch.manual_seed(42)
+    #X is data Y is label
+    trX, trY, teX, teY, valX, valY = load_marchmadness()
+    
+    n_examples, n_features = list(trX.size())[0], list(trX.size())[1]
+    n_classes = 2
+    model = build_model(n_features, n_classes)
+    loss = torch.nn.CrossEntropyLoss(reduction='elementwise_mean')
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    batch_size = 100
+
+    for i in range(100):
+        cost = 0.
+        num_batches = n_examples // batch_size
+        for k in range(num_batches):
+            start, end = k * batch_size, (k + 1) * batch_size
+            cost += train(model, loss, optimizer,
+                          trX[start:end], trY[start:end])
+        predY = predict(model, teX)
+        print("Epoch %d, cost = %f, acc = %.2f%%"
+              #.cpu().numpy() turns it back into a numpy array
+              % (i + 1, cost / num_batches, 100. * np.mean(predY == teY.cpu().numpy())))
+
+if __name__ == "__main__":
+    main()
