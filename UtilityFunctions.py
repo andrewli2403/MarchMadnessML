b7e09a0290abbfa5bd8de5787e6968a0568c79c1
Andrew Li <Andrew@Andrews-Air.attlocal.net>
2020-02-16 09:34:20 -0800

    new utility

--- UtilityFunctions.py
+++ UtilityFunctions.py
@@ -1,4 +1,4 @@
-#
+#randomize labels
 def randomization(labelList, originalDataSet):
     randomizedDataSet = []
     for i, do_flip in enumerate(labelList):    
@@ -11,10 +11,53 @@ def randomization(labelList, originalDataSet):
         randomizedDataSet.append(x_rand)
     return randomizedDataSet
 
-#
+#turn into pytorch array
 def PytorchArray (labels, randomizedDataSet):
     randomizedDataSet_np = np.array(randomizedDataSet).astype(np.float32)
     labels_np = np.array(labels)
     randomizedDataSet_pytorch = torch.from_numpy(randomizedDataSet_np)
     labels_pytorch = torch.from_numpy(labels_np)    
     return randomizedDataSet_pytorch, labels_pytorch
+
+#load in processed data from CollegeBasketballPredictor.ipynb
+def load_marchmadness():
+    #features from training, test, validation sets
+    featurestrainingData = torch.load('featurestraining_pytorch.pt')
+    featurestestData = torch.load('featurestest_pytorch.pt')
+    featuresvalidationData = torch.load('featuresvalidation_pytorch.pt')
+    
+    #labels from training, test, validation sets
+    labelstrainingData = torch.load('labelstraining_pytorch.pt')
+    labelstestData = torch.load('labelstest_pytorch.pt')
+    labelsvalidationData = torch.load('labelsvalidation_pytorch.pt')
+    
+    return featurestrainingData, labelstrainingData, featurestestData, labelstestData, featuresvalidationData, labelsvalid
+
+def build_model(input_dim, output_dim):
+    # We don't need the softmax layer here since CrossEntropyLoss already
+    # uses it internally.
+    model = torch.nn.Sequential()
+    model.add_module("linear",
+                     torch.nn.Linear(input_dim, output_dim, bias=False))
+    return model
+
+def train(model, loss, optimizer, x, y):
+    # Reset gradient
+    optimizer.zero_grad()
+
+    # Forward
+    fx = model.forward(x)
+    output = loss.forward(fx, y)
+
+    # Backward
+    output.backward()
+
+    # Update parameters
+    optimizer.step()
+
+    return output.item()
+
+def predict(model, x):
+    output = model.forward(x)
+    return output.data.numpy().argmax(axis=1)
+
