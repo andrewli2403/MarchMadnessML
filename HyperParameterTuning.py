307ab3443e77b5d6b5df2e650bc0aab1cde09cb7
Andrew Li <Andrew@Andrews-Air.attlocal.net>
2020-03-07 20:49:10 -0800

    found opitmal lr

--- HyperParameterTuning.py
+++ HyperParameterTuning.py
@@ -1,5 +1,5 @@
-#collect all 100 epochs of the number and store number into array
-#use function and return the biggest value in the array
+#collect all 100 epochs of the lr and determine the max accuracy
+#repeat for rest of lr and return the max accuracy from a certain lr
 
 import numpy as np
 
@@ -12,10 +12,11 @@ from UtilityFunctions import build_model
 from UtilityFunctions import train
 from UtilityFunctions import predict
 
-#maybe its the cost
+#learning rate list
 lr_list = [0.0001, 0.001, 0.01, 0.1, 1.0]
+result_list = []
 
-def main():
+def main(lr_value):
     torch.manual_seed(42)
     #X is data Y is label
     trX, trY, teX, teY, valX, valY = load_marchmadness()
@@ -24,28 +25,45 @@ def main():
     n_classes = 2
     model = build_model(n_features, n_classes)
     loss = torch.nn.CrossEntropyLoss(reduction='elementwise_mean')
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    optimizer = optim.SGD(model.parameters(), lr= lr_value, momentum=0.9)
     batch_size = 100
 
+    acc = []
+    max_lr = 0;
     for i in range(100):
         cost = 0.
         num_batches = n_examples // batch_size
+        #use validation set
         for k in range(num_batches):
             start, end = k * batch_size, (k + 1) * batch_size
             cost += train(model, loss, optimizer,
                           trX[start:end], trY[start:end])
-        #use validation set
+    
         predY = predict(model, valX)
-        #changed print to return
-        return("Epoch %d, cost = %f, acc = %.2f%%"
+        
+        #collects accuracy for each epoch
+        acc.append(100. * np.mean(predY == valY.cpu().numpy()))
+        
+        print("Epoch %d, cost = %f, acc = %.2f%%"
               #.cpu().numpy() turns it back into a numpy array
+              #each refers to the printed values
               % (i + 1, cost / num_batches, 100. * np.mean(predY == valY.cpu().numpy())))
 
-result_list = []
+#finds maximum accuracy value and adds to result_list
+    max_lr = max(acc)
+    result_list.append(max_lr)
+
 if __name__ == "__main__":
-    main()
-    
-    
-    #for i in lr_list:
-    #result = main(lr_list[i])
-#result_list.append(result)
+    for value in lr_list:
+        main(value)
+
+#associates learning rate and its respective maximum accuracy
+#{learning rate: accuracy}
+lr_acc = {}
+for i in range(len(lr_list)):
+    lr_acc.update({lr_list[i]:result_list[i]})
+print(lr_acc)
+
+#finds the learning rate that has the highest accuracy
+optimal_lr = max(lr_acc, key=lr_acc.get)
+print(optimal_lr)
